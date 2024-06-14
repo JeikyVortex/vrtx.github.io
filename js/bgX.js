1b4c110fe0045109d3ec48a083d3a1bc60d9caa2
Joaquin Rojas Muñoz <jeikolook@gmail.com>
2024-06-14 12:18:32 -0400

    Add files via upload

---
+++ js/bgX.js
@@ -0,0 +1,8 @@
+function tekken() {
+
+const tekken = new Audio('./audio/bg.mp3');
+tekken.volume = 0.50
+tekken.play();
+}
+
+tekken();
---
+++ js/secrets.js
@@ -0,0 +1,78 @@
+function heads() {
+  var audio = new Audio('./audio/tada.mp3');
+  audio.play();
+  setTimeout(headsmsg, 1000);
+}
+
+function headsmsg() {
+  alert("can u save...");
+  alert("can u save my...");
+  alert("CAN U SAVE MY-");
+
+  window.location.href = "https://www.youtube.com/watch?v=r_9Kf0D5BTs";
+}
+
+function tails() {
+  var audio = new Audio('./audio/tada.mp3');
+  audio.play();
+  setTimeout(tailsmsg, 1000);
+}
+
+function tailsmsg() {
+  alert("HEY KID, GET OUT OF THE ROAD!!1");
+
+  window.location.href = "https://www.youtube.com/watch?v=a5i-KdUQ47o";
+}
+
+function russian() {
+  var audio = new Audio('./audio/tada.mp3');
+  audio.play();
+  setTimeout(roulette, 1000);
+}
+
+function roulette() {
+  alert("dont kill yourself");
+  alert("you idiot");
+
+  window.location.href = "https://www.youtube.com/embed/ijJorw80_7g?autoplay=1";
+}
+
+function blossom() {
+  var audio = new Audio('./audio/tada.mp3');
+  audio.play();
+  setTimeout(blossommsg, 1000);
+}
+
+function blossommsg(){
+  alert("Lo siento por llorar");
+  alert("Es que te amo mucho");
+  window.location.href = "https://www.youtube.com/embed/XeaEvZgTKxY?autoplay=1";
+}
+
+function jeiky() {
+  var audio = new Audio('./audio/tada.mp3');
+  audio.play();
+  setTimeout(vortex, 1000);
+}
+
+function vortex(){
+  alert("Tenia que tener una password yo, no?");
+  window.location.href = "https://www.youtube.com/watch?v=AupwoN8QvbU";
+}
+
+function playme() {
+  var audio = new Audio('./audio/tada.mp3');
+  audio.play();
+  setTimeout(rick, 1750);
+  setTimeout(rickmsgs1, 11550);
+}
+
+function rick(){
+  var audio = new Audio('./audio/rickroll.mp3');
+}
+
+function rickmsgs1(){
+  alert("Me van a creer que mi sueño cuando niño era hacer ESTA broma de ESTA forma?");
+  alert("Estoy muy feliz haciendo estas cosas. (┬┬◡┬┬) 💕");
+  alert("Es el tema entero, por cierto, si quieres parar el tema, presiona F5");
+}
